cdcef30b974fc1e9255ae1ee23b58c0b6c3393f1
Kevin2133 <68924818+Kevin2133@users.noreply.github.com>
2022-04-27 12:33:16 +0200

    Add files via upload

--- arrayOOP1/src/App.java
+++ arrayOOP1/src/App.java
@@ -1,3 +1,8 @@
+/**
+ * 5. Primo cognome in ordine alfabetico
+ * 6. Cognome piu' lungo
+ * 7. Quanti cognomi ci sono che iniziano per A?
+ */
 import java.util.*;
 public class App {
 
@@ -11,6 +16,17 @@ public class App {
         return -1;
     }
 
+    static int quantiCogPerSubs (Contatti[] rubrica, int at, String subsl, int cont){
+        int k = 0;
+        for(int i = 0; i < cont; i++){
+            if(rubrica[i].cognome.substring(at, at + subsl.length()).toLowerCase().equals(subsl)){
+                k++;
+            }
+        }
+
+        return k;
+    }
+
     public static void main(String[] args) throws Exception {
         Contatti rubrica[] = new Contatti[1000];
         int cont = 0;
@@ -18,7 +34,7 @@ public class App {
         String nome, cognome, cellulare;
         int annoNascita;
         int annoCorrente, eta;
-        int ind;
+        int ind; String primoCog;
         Scanner scanner = new Scanner(System.in);
 
         do{
@@ -27,6 +43,9 @@ public class App {
             System.out.println("2. Visualizza contatti");
             System.out.println("3. Calcola eta di tutti i contatti");
             System.out.println("4. Eta di un contatto per nome e cognome");
+            System.out.println("5. Primo cognome ordine alfabetico");
+            System.out.println("6. Cognome piu' lungo");
+            System.out.println("7. Quanti cognomi con lettera A");
 
             scelta = scanner.nextInt();
             scanner.nextLine();
@@ -91,6 +110,63 @@ public class App {
                         }else{
                             System.out.println("Contatto non trovato");
                         }
+                    }else{
+                        System.out.println("Non ci sono elementi nell'array");
+                    }
+                    break;
+                case 5:
+                    if(cont > 0){
+                        primoCog = rubrica[0].cognome;
+                        for(int i = 0; i < cont; i++){
+                            if(rubrica[i].cognome.compareTo(primoCog) < 0){
+                                primoCog = rubrica[i].cognome;
+                            }
+                        }
+
+                        System.out.println("Primo cognome in ordine alfabetico= " + primoCog);
+                    }else{
+                        System.out.println("Non ci sono elementi nell'array");
+                    }
+                    break;
+                case 6:
+                    if(cont > 0){
+                        primoCog = rubrica[0].cognome;
+                        for(int i = 0; i < cont; i++){
+                            if(rubrica[i].cognome.length() > primoCog.length()){
+                                primoCog = rubrica[i].cognome;
+                            }
+                        }
+
+                        System.out.println("Cognome piu' lungo= " + primoCog);
+                    }else{
+                        System.out.println("Non ci sono elementi nell'array");
+                    }
+                    break;
+                case 7:
+                    if(cont > 0){
+                        int k = 0;
+                        for(int i = 0; i < cont; i++){
+                            //if(rubrica[i].cognome.substring(0, 1).toLowerCase().equals("a")){
+                                //k++;
+                            //}
+
+                            if(rubrica[i].cognome.charAt(0) == 'A'){
+                                k++;
+                            }
+
+                        }
+
+                        System.out.println("Cognomi che iniziano con la a= " + k);
+                    }else{
+                        System.out.println("Non ci sono elementi nell'array");
+                    }
+                    break;
+                case 71:
+                    if(cont > 0){
+                        int num;
+                        num = quantiCogPerSubs(rubrica, 0, "a", cont);
+                        
+                        System.out.println("Cognomi che iniziano con la A= " + num);
                     }else{
                         System.out.println("Non ci sono elementi nell'array");
                     }
